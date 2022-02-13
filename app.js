abbef1290353978a53aacdd9c8f50b3c92faf900
Sachin Sharma <sachinmodgil6@gmail.com>
2022-02-13 08:58:19 +0530

    added jquery and miror tweeks

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const btnPanel = document.getElementById("buttons");
+const redeemBTN = document.getElementById("redeemBTN");
+const transferBTN = document.getElementById("transferBTN");
+const editBTN = document.getElementById("editBTN");
+
+// Scripts
+
+$("#redeemBTN").click(function(){
+    alert("Redeem Button Clicked");
+});
+
+$("#transferBTN").click(function(){
+    alert("Transfer Button Clicked");
+});
+
+$("#editBTN").click(function(){
+    alert("Edit Button Clicked");
+})
